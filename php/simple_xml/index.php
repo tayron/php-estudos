9d5343a69a44cae7bba50c6cc2f620d22fab28ce
Tayron Miranda <dev@tayron.com.br>
2016-06-10 04:55:49 -0300

    Update index.php

--- php/simple_xml/index.php
+++ php/simple_xml/index.php
@@ -4,3 +4,23 @@ $books = simplexml_load_file('books.xml');
 foreach ($books as $book) {
     echo "$book->title - $book->author - $book->publisher <br />";
 }
+
+
+$xml = "<books>
+    <book>
+        <author>Jack Herrington</author>
+        <title>PHP Hacks</title>
+        <publisher>O'Reilly</publisher>
+    </book>
+    <book>
+        <author>Jack Herrington</author>
+        <title>Podcasting Hacks</title>
+        <publisher>O'Reilly</publisher>
+    </book>
+</books>";
+
+$books = simplexml_load_string($xml);
+
+foreach ($books as $book) {
+    echo "$book->title - $book->author - $book->publisher <br />";
+}
